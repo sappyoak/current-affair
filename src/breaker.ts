582dbb4603108497ec5acd531bdcfc19228fbed7
Forrest Collins <81883643+sappyoak@users.noreply.github.com>
2022-06-03 19:50:10 -0500

    Includes Defined States for the breaker

--- src/breaker.ts
+++ src/breaker.ts
@@ -7,10 +7,26 @@ import { EventEmitterAsyncResource } from 'node:events'
 
 import { IBreakerOptions, createBreakerOptions } from './options'
 
+export enum BreakerStates {
+    /** Normal Operation. Actions Execute normally */
+    Closed,
+    /** The Circuit is open, actions will fail when executed */
+    Open,
+    /** We are in a healing state after the designated {halfOpenTimeout}. Execution of one action
+     *  is permitted to determine whether the breaker should transition to a closed (successful) or
+     *  back to open open (failed) state */
+    HalfOpen,
+    /** The Breaker is removed from the success/failure context and is manually held in an open state
+     *  where all actions will fail */
+    Detached
+}
+
 export class Breaker extends EventEmitterAsyncResource {
     readonly options: IBreakerOptions
     readonly name: string
     readonly group: string
+    readonly enabled: boolean
+    readonly state: number = BreakerStates.Closed
 
     constructor(passedOptions: Partial<IBreakerOptions> = {}) {
         const options = createBreakerOptions(passedOptions)
@@ -19,5 +35,6 @@ export class Breaker extends EventEmitterAsyncResource {
         this.options = options
         this.name = options.name
         this.group = options.group
+        this.enabled = options.enabled
     }
 }
