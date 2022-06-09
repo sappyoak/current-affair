5acbdec834dc2bd50eaa4df1e6e63b21fd517f70
Forrest Collins <81883643+sappyoak@users.noreply.github.com>
2022-06-08 20:44:14 -0500

    Creates a Base command class similar to what a Hystrix Command would be that orchestrates this moving pieces for a specific action

---
+++ src/command.ts
@@ -0,0 +1,35 @@
+import { EventEmitter } from 'node:events'
+
+import { IConfiguration, createConfiguration } from './config'
+import { Executor } from './executor'
+import { Metrics } from './Metrics'
+import { Semaphore } from './semaphore'
+
+export class Command extends EventEmitter {
+    protected config: IConfiguration
+    protected name: string
+    protected group: string
+    protected commandState
+    protected executor: Executor
+    protected metrics: Metrics
+    protected semaphore: Semaphore
+
+    public constructor(options: Partial<IConfiguration>) {
+        super()
+
+        this.config = createConfiguration(options)
+        this.name = this.config.name
+        this.group = this.config.group
+
+        this.executor = new Executor(this.config.actionTimeout, this.config.errorFilter)
+        this.metrics = new Metrics({
+            rollingWindowDuration: this.config.rollingWindowDuration,
+            rollingBucketCount: this.config.rollingBucketCount,
+            calculateRollingPercentile: this.config.calculateRollingPercentile,
+            percentiles: this.config.percentiles,
+            snapshotInterval: this.config.snapshotInterval
+        })
+        this.semaphore = new Semaphore(this.config.maxActionConcurrency, this.config.maxActionQueueSize)
+        
+    }
+}
